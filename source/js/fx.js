9bb50f93ad0c3872b773b71f15b6716bb5e27bae
Chad <chad@smithandrobot.com>
2011-02-07 16:31:03 -0600

    finished modal windows for all but photo

--- source/js/fx.js
+++ source/js/fx.js
@@ -20,6 +20,28 @@ $(function()
 	
 });
 
+// tweet box styling
+
+function styleTweetBox()
+{
+label = $("#tbox iframe").contents().find("label");
+counter = $("#tbox iframe").contents().find("#counter");
+var fontSize = label.css('fontSize');
+//alert('styling tweet box: '+label.html());
+label.css('font-size', 16);
+label.css('color', "#ccc");
+counter.css('position', 'absolute');
+counter.css('color', "#666");
+counter.css('left', '260px');
+counter.css('top', '0px');
+counter.css('text-align', 'right');
+counter.css('width', 80);
+counter.css('font-size', 16);
+}
+
+
+// modal managers -- action-follow
+
 $(document).ready(function() {
 			init();
 		});
@@ -28,7 +50,7 @@ $(document).ready(function() {
 		{
 			// follow
 			$('.expert-bio').qtip({
-			   content: $('#modal-bio').html(),
+			   content: $('.modal-bio').html(),
    				position: { 
 					corner: { target: 'topLeft', tooltip: 'bottomLeft'},
 					adjust: { x: -18, y: 20 }
@@ -43,6 +65,66 @@ $(document).ready(function() {
 			hide: {
             	fixed: true
          	},
-			show:{when:'click'}
+			show:{delay: 0, when:'click'}
+			});
+			
+			// follow
+			$('.action-follow').qtip({
+			   content: $('.modal-follow').html(),
+   				position: { 
+					corner: { target: 'topLeft', tooltip: 'bottomLeft'},
+					adjust: { x: -40, y: 20 }
+				},
+			    style: {
+			    	tip: false,
+					background: 'none',
+                	padding: 0,
+					border: false,
+				 	width: { min: 294 }
+			   },
+			hide: {
+            	fixed: true
+         	},
+			show:{delay: 0, when:'click'}
+			});
+			
+			// retweet -- modal-tweetbox
+			$('.action-retweet').qtip({
+			   content: $('.modal-retweet').html(),
+   				position: { 
+					corner: { target: 'topLeft', tooltip: 'bottomLeft'},
+					adjust: { x: -40, y: 20 }
+				},
+			    style: {
+			    	tip: false,
+					background: 'none',
+                	padding: 0,
+					border: false,
+				 	width: { min: 294 }
+			   },
+			hide: {
+            	fixed: true
+         	},
+			show:{delay: 0, when:'click'}
+			});
+			
+			// reply modal-tweetbox
+			$('.action-reply').qtip({
+			   content: $('.modal-reply').html(),
+   				position: { 
+					corner: { target: 'topLeft', tooltip: 'bottomLeft'},
+					adjust: { x: -40, y: 20 }
+				},
+			    style: {
+			    	tip: false,
+					background: 'none',
+                	padding: 0,
+					border: false,
+				 	width: { min: 294 }
+			   },
+			hide: {
+            	fixed: true
+         	},
+			show:{delay: 0, when:'click'}
 			});
 		}
