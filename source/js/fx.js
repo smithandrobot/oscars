1454dd301dc5cc018f3668bdc46d45973a3ed6d8
Chad <chad@smithandrobot.com>
2011-02-07 09:16:35 -0600

    adding modal windows

--- source/js/fx.js
+++ source/js/fx.js
@@ -19,3 +19,30 @@ $(function()
 		);
 	
 });
+
+$(document).ready(function() {
+			init();
+		});
+		
+		function init()
+		{
+			// follow
+			$('.expert-bio').qtip({
+			   content: $('#modal-bio').html(),
+   				position: { 
+					corner: { target: 'topLeft', tooltip: 'bottomLeft'},
+					adjust: { x: -18, y: 20 }
+				},
+			    style: {
+			    	tip: false,
+					background: 'none',
+                	padding: 0,
+					border: false,
+				 	width: { min: 294 }
+			   },
+			hide: {
+            	fixed: true
+         	},
+			show:{when:'click'}
+			});
+		}
